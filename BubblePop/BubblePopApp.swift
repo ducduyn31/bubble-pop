2d8f355f60828565753877e3d85be5f096b71ff1
ducduyn31 <ducduy.ababat@gmail.com>
2024-04-15 23:42:40 +1000

    Initial Commit

---
+++ BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by Duy Nguyen on 15/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
